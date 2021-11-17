9520617279b8e0294e792170e2aab682b2e1a350
Caitlin Roberts <caitlin.coy.roberts@gmail.com>
2021-11-17 14:08:10 -0700

    refined function for retrieving venues from api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,43 @@
 //VARIABLE DECLARATIONS
 
 //FUNCTION DECLARATIONS
+
+//General function for API retrieval
 function apiFetch(requestUrl) {
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    });
+  var apiData;
+  //     .then(function (data) {
+  //       apiData = data;
+  //     });
+  //   return apiData;
 }
 
+//Retrieves an array of objects for 10 venues
 function cityInput(event) {
   event.preventDefault();
   var userCity = document.getElementById("inputPassword2");
 
   var userInput = userCity.value;
   var requestUrl = `https://api.seatgeek.com/2/venues?city=${userInput}&client_id=MjQ0ODg0NjR8MTYzNzA5NzUyOC4xOTgwMTUy`;
-  apiFetch(requestUrl);
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      //   apiData = data.venues;
+      console.log(data.venues);
+      for (var i = 0; i < data.venues.length; i++) {
+        console.log(data.venues[i]);
+      }
+    });
+  //   console.log(apiFetch(requestUrl));
 }
 
+// var venueArray;
+// var venueName;
+// var venueAddress;
+// var venueUrl;
+
 //EVENT HANDLERS
+//Listens for a click on the submit button
 document.querySelector("#submit").addEventListener("click", cityInput);
