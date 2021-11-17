e0dd94871726675d67d5575619ff7b779d7ea5d4
Caitlin Roberts <caitlin.coy.roberts@gmail.com>
2021-11-17 12:41:11 -0700

    added event listener to pull venues from api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,24 @@
-gia;
+//VARIABLE DECLARATIONS
+
+//FUNCTION DECLARATIONS
+function apiFetch(requestUrl) {
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
+
+function cityInput(event) {
+  event.preventDefault();
+  var userCity = document.getElementById("inputPassword2");
+
+  var userInput = userCity.value;
+  var requestUrl = `https://api.seatgeek.com/2/venues?city=${userInput}&client_id=MjQ0ODg0NjR8MTYzNzA5NzUyOC4xOTgwMTUy`;
+  apiFetch(requestUrl);
+}
+
+//EVENT HANDLERS
+document.querySelector("#submit").addEventListener("click", cityInput);
